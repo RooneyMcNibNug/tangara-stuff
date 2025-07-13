f237dcc18ff051d856c3eeb960b772dc43c1be70
JA <Jakea350@protonmail.com>
2025-07-13 07:39:31 -0500

    Update cleanup.sh to preserve .playlist files

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,6 @@
 metaflac --remove --block-type=PICTURE,PADDING --dont-use-padding ./*/*.flac
 
 # Find and remove all files that are not FLAC, MP3, or M4A
-find ./*/* -type f ! -name "*.flac" ! -name "*.mp3" ! -name "*.m4a" -exec sh -c 'echo "Deleting: $1"; rm -f "$1"' _ {} \;
+find ./*/* -type f ! -name "*.flac" ! -name "*.mp3" ! -name "*.m4a" ! -name "*.playlist" -exec sh -c 'echo "Deleting: $1"; rm -f "$1"' _ {} \;
 
 echo "Cleanup is complete - enjoy your tunes!"
